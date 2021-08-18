0ff2aecb71347f67b92814056568c3e03c1ea232
stinkyfingers <john_shenk@hotmail.com>
2021-08-18 18:37:46 -0400

    adds calendar modal

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,9 +1,28 @@
 import React  from 'react';
+import Modal from 'react-modal';
 import { getEvents } from '../api';
+import logo from '../images/fh_logo.png';
 import '../css/calendar.css';
+
+const modalStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    color: 'white',
+    backgroundColor: '#333',
+    padding: '2em',
+    'line-height': '2'
+  },
+};
+
 
 const Calendar = () => {
   const [events, setEvents] = React.useState([]);
+  const [event, setEvent] = React.useState(null);
   React.useEffect(() => {
     getEvents().then((resp) => {
       setEvents(resp);
@@ -26,28 +45,47 @@ const Calendar = () => {
 
   const renderEvents = () => {
     const data = events.map(e => (
-      <tr className='event' key={e.id}>
+      <tr className='event' key={e.id} onClick={() => setEvent(e)}>
         <td>{e.summary}</td>
         <td>{formatDate(e.start.dateTime, e.end.dateTime)}</td>
-        <td>{e.description}</td>
+        <td>{e.location}</td>
       </tr>))
       return <table>
         <thead>
           <tr>
             <th>Event</th>
             <th>Date</th>
-            <th>Details</th>
+            <th>Location</th>
           </tr>
         </thead>
         <tbody>
           {data}
         </tbody>
       </table>;
-  }
+  };
+
+  const renderEvent = () => {
+    if (!event) return null;
+    return <div className='event'>
+      <div><img src={logo} alt='False Heather' className='modalLogo'/></div>
+      <div>Event: {event.summary}</div>
+      <div>Date: {formatDate(event.start.dateTime, event.end.dateTime)}</div>
+      <div>Location: {event.location}</div>
+      <div>Description: {event.description}</div>
+    </div>
+  };
 
   return (
     <div className='calendar'>
       {renderEvents()}
+      <Modal
+        isOpen={event !== null}
+        style={modalStyles}
+        onRequestClose={() => setEvent(null)}
+        contentLabel={event ? event.summary : ''}
+      >
+        { event ? renderEvent() : null }
+      </Modal>
     </div>
   );
 };
