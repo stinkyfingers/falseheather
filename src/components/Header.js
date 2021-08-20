45f34c87be532784f01ade6d515caebd9c17a028
stinkyfingers <john_shenk@hotmail.com>
2021-08-20 16:26:56 -0400

    adds fb

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../css/header.css';
 import logo from '../images/fh_logo.png';
+import fb from '../images/Facebook.png';
+import music from '../images/music.png';
 
 const Header = () => {
   return (<div className='header'>
-    <Link to='/music' className='link'>Music</Link>
-    <Link to='/'><img src={logo} alt='False Heather' className='logo' /></Link>
-    <Link to='/' className='link'></Link>
+    <Link to='/music' title='Music'><img src={music} alt='Music' className='logo' /></Link>
+    <Link to='/' title='Calendar'><img src={logo} alt='False Heather' className='logo' /></Link>
+    <a href='https://www.facebook.com/FLSHeather' title='Goddamned Facebook'><img src={fb} alt='Facebook' className='facebook' /></a>
   </div>);
 }
 
