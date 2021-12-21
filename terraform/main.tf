b17000a8e219c08a905c9c62dc6e840c04b0179b
stinkyfingers <john_shenk@hotmail.com>
2021-12-21 16:21:21 -0600

    ?

--- terraform/main.tf
+++ terraform/main.tf
@@ -90,9 +90,10 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   }
 
   custom_error_response {
-    error_code = 404
-    response_code = 200
-    response_page_path = "/index.html"
+    error_caching_min_ttl = 3000
+    error_code            = 404
+    response_code         = 200
+    response_page_path    = "index.html"
   }
 
   restrictions {
