3bc6db41903b99a8142d019c07b0ef7d2882fe23
stinkyfingers <john_shenk@hotmail.com>
2021-12-21 16:08:29 -0600

    s3 redirect

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,6 +15,16 @@ resource "aws_s3_bucket" "falseheather" {
   website {
     index_document = "index.html"
     error_document = "index.html"
+    routing_rules = <<EOF
+[{
+"Condition": {
+    "KeyPrefixEquals": "msuic/"
+},
+"Redirect": {
+    "ReplaceKeyPrefixWith": "/"
+}
+}]
+EOF
   }
 }
 
